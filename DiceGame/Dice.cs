304cd648df9e0cb93a5b630a24874b76eceb3520
CUCHO20 <chu2cho.63@gmail.com>
2025-05-24 01:13:57 -0500

    Added Dice class

---
+++ DiceGame/Dice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    public class Dice
+    {
+        public string Name { get; }
+        public IReadOnlyList<int> Faces { get; }
+
+        public Dice(int[] faces)
+        {
+            Faces = new List<int>(faces).AsReadOnly();
+            Name = $"[{string.Join(",", Faces)}]";
+        }
+
+        public int Roll(int faceInx)
+        {
+            return Faces[faceInx];
+        }
+    }
+}
