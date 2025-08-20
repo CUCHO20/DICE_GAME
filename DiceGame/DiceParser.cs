28d86f13e49d96204d6e0123abc34ba221abef7d
CUCHO20 <chu2cho.63@gmail.com>
2025-08-19 22:36:13 -0500

    ADDED DICE PARSER FOR PARSING DICE ELEMENTS WITHIN ARRAY [DiceParser]

---
+++ DiceGame/DiceParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+    public class DiceParser
+    {
+        public List<Dice> Parse(string[] args)
+        {
+            try {
+                var result = new List<Dice>();
+                foreach(var arg in args)
+                {
+                    var faces = Array.ConvertAll(arg.Split(','), int.Parse);
+                    result.Add(new Dice(faces));
+                }
+                return result;
+            } catch {
+                return null;
+            }
+        }
+    }
+}
