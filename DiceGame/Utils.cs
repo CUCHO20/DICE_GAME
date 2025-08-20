330e42d9f3380f68697713aebdb8b9941afeb1a1
CUCHO20 <chu2cho.63@gmail.com>
2025-08-20 00:17:28 -0500

    ADDED UTILITIES FOR RANDOM SET OF MOVEMENTS [Utils]

---
+++ DiceGame/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+    internal class Utils
+    {
+        public static int SecureRandomInt(int minInclusive, int maxExclusive) {
+            var rng = RandomNumberGenerator.Create();
+            byte[] data = new byte[4];
+            rng.GetBytes(data);
+            int value = BitConverter.ToInt32(data, 0) & int.MaxValue;
+            return value % (maxExclusive - minInclusive) + minInclusive;
+        }
+
+        public static int Modulo(int x, int m)
+        {
+            return (x % m + m) % m;
+        }
+    }
+}
