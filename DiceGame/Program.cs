e716319c29b3810c17f8ebd301938a4628aa4ef2
CUCHO20 <chu2cho.63@gmail.com>
2025-08-21 00:23:34 -0500

    MODIFIED Main CLASS TO RUN PROGRAM [Program]

--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Usage: dotnet run \"face1,face2,...\" \"face1,face2,...\" ...");
+                Console.WriteLine("Example: dotnet run \"2,2,4,4,9,9\" \"6,8,1,1,8,6\" \"7,5,3,7,5,3\"");
+                return; 
+            }
+
+            var parser = new DiceParser();
+            var diceList = parser.Parse(args);
+
+            if (diceList == null)
+            {
+                Console.WriteLine("Invalid input. Please ensure all arguments are comma-separated integers.");
+                return;
+            }
+
+            var game = new GameEngine(diceList);
+            game.Play();
         }
     }
 }
